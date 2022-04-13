45906c6b544a026c05bc65a04e0590b6d758338a
Ayoub <ayoubalbez1@gmail.com>
2022-04-13 02:41:38 +0200

    similar recipes

---
+++ app/src/main/java/com/example/mealfinder1/Model/SimilarRecipesListener.java
@@ -0,0 +1,9 @@
+package com.example.mealfinder1.Model;
+
+import java.util.List;
+
+public interface SimilarRecipesListener {
+    void didFetch(List<SimilarRecipeResponse> response, String message);
+    void didError(String message);
+
+}
