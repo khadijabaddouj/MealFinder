25b194413704c9f8d1e944a7662c090fee37fb28
yahya benzha <yahya_benzha@um5.ac.ma>
2022-04-25 22:15:12 +0000

    settings conf

---
+++ app/src/main/java/com/example/mealfinder1/Model/RecipeSmall.java
@@ -0,0 +1,8 @@
+package com.example.mealfinder1.Model;
+
+public class RecipeSmall {
+    public int id;
+    public String title;
+    public String image;
+    public String imageType;
+}
---
+++ app/src/main/java/com/example/mealfinder1/RecipeDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.mealfinder1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+public static int id;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+    }
+}
